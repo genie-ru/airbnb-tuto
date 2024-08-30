05650217d22e50f916fcf0252dd407d4b309fb59
[genie-ru] <[genie.zgmf987@gmail.com]>
2024-08-30 12:08:37 +0900

    グリッドレイアウトを設定し、複数の画像をレスポンシブに表示

--- app/tours/page.tsx
+++ app/tours/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 const url = 'https://www.course-api.com/react-tours-project';
 
 type Tour = {
@@ -21,13 +22,26 @@ async function TourPage() {
   return (
     <section>
       <h1 className="text-3xl mb-4">Tours</h1>
+      <div className="grid md:grid-cols-2 gap-8">
+
       {data.map((tour) => {
         return (
           <Link key={tour.id} href={`/tours/${tour.id}`} className='hover:text-blue-500'>
+            <div className='relative h-48 mb-2'>
+              <Image 
+                src={tour.image} 
+                alt={tour.name} 
+                fill 
+                sizes='(max-width:768px) 100vw, (max-width:1200px) 50vw,33vw'
+                priority 
+                className="object-cover rounded"
+              />
+            </div>
             <h2>{tour.name}</h2>
           </Link>
         );
       })}
+      </div>
     </section>
   )
 }
