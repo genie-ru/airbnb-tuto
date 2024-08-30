31f72c5d3fce798e3652ad42cf72aa28c42a3f2f
[genie-ru] <[genie.zgmf987@gmail.com]>
2024-08-30 11:21:05 +0900

    add img_file

--- app/tours/[id]/page.tsx
+++ app/tours/[id]/page.tsx
@@ -1,8 +1,23 @@
+import mapsImg from '@/images/maps.jpeg';
+import Image from 'next/image';
 
 function page({params}:{params:{id:string}}) {
   return (
     <div>
       <h1 className="text-4xl">ID : {params.id}</h1>
+      <section className='flex gap-x-4 mt-4'>
+        <div>
+          <Image
+            src={mapsImg}
+            alt='maps' 
+            width={192} 
+            height={192} 
+            className='w-48 h-48 object-cover rounded' 
+          />
+          <h2>local image</h2>
+        </div>
+        <div></div>
+      </section>
     </div>
   )
 }
