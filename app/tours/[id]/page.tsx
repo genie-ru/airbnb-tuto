2f77051bd9cc37dedd05934f8c56c04edc0531c3
[genie-ru] <[genie.zgmf987@gmail.com]>
2024-08-30 11:56:11 +0900

    にホワイトリストとしてサポートされるURLパターンを設定

--- app/tours/[id]/page.tsx
+++ app/tours/[id]/page.tsx
@@ -1,5 +1,6 @@
 import mapsImg from '@/images/maps.jpeg';
 import Image from 'next/image';
+const url = 'https://www.course-api.com/images/tours/tour-1.jpeg';
 
 function page({params}:{params:{id:string}}) {
   return (
@@ -16,7 +17,16 @@ function page({params}:{params:{id:string}}) {
           />
           <h2>local image</h2>
         </div>
-        <div></div>
+        <div>
+          <Image
+            src={url}
+            alt='tour' 
+            width={192} 
+            height={192} 
+            className='w-48 h-48 object-cover rounded'
+          />
+          <h2>remote image</h2>
+        </div>
       </section>
     </div>
   )
