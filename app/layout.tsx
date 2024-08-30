54c2863187e6607ec234fae78a9b26475ac3742a
[genie-ru] <[genie.zgmf987@gmail.com]>
2024-08-30 17:13:39 +0900

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar';
 import './globals.css';
-
+import { Inter } from 'next/font/google';
+const inter = Inter({subsets: ['latin']});
 export default function RootLayout({
   children,
 }: {
@@ -8,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body>
+      <body className={inter.className}>
         <Navbar />
         <main className='max-w-3xl mx-auto py-10'>
           {children}
